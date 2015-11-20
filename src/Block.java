dbcc1362a99864e54b545bce49a436360636c228
ajguzman <ajguzman09@gmail.com>
2015-11-19 22:30:00 -0800

    COMMENTS

--- src/Block.java
+++ src/Block.java
@@ -4,6 +4,7 @@ public class Block {
     private BlockType Type;
     private float x,y,z;
     
+    
     public enum BlockType {
         BlockType_Grass(0),
         BlockType_Sand(1),
@@ -16,23 +17,37 @@ public class Block {
         private int BlockID; 
         
         BlockType(int i) { BlockID = i; }
-
+        
+        // method: GetID
+        // purpose: this method returns the BlockType ID
         public int GetID(){ return BlockID; }
-
+        // method: SetID
+        // purpose: this method sets BlockType ID
         public void SetID(int i){ BlockID = i; }
     }
-    
+    // method: SetID
+    // purpose: this method sets the block ID
+    // to be one of the 6 types
     public Block(BlockType type){ Type= type; }
     
+    //method: SetID
+    //purpose: this method sets location of the block.
+    //Note: Not used when rendering in Chunks
     public void setCoords(float x, float y, float z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
-    
+    // method: IsActive
+    // purpose: this metod returns true if the current block
+    // should be rendered anf false if excluded fron rendering
     public boolean IsActive() { return IsActive; }
     
+    // method: SetActive
+    // purpose: this metod sets the visibility of the block
     public void SetActive(boolean active) { IsActive=active; }
-    
+    // method: GetID
+    // purpose: this method returns the block ID to identify
+    // what type of block type will be rendered
     public int GetID() { return Type.GetID(); }
 }
